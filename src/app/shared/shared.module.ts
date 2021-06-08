6e3a3a0eb9808e49049355841a6c569d2b53fb69
Michael Cagampan <johnmichaelronn.cagampan@concentrix.com>
2021-06-09 06:37:02 +0800

    updated shared module, integrated ant design ui framework

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,22 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { JcvaInputComponent } from './components/jcva-input/jcva-input.component';
 
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+
 @NgModule({
   declarations: [JcvaInputComponent],
   imports: [CommonModule, ReactiveFormsModule, FormsModule],
-  exports: [ReactiveFormsModule, FormsModule, JcvaInputComponent],
+  exports: [
+    ReactiveFormsModule,
+    FormsModule,
+    JcvaInputComponent,
+    NzDropDownModule,
+    NzIconModule,
+    NzCardModule,
+    NzMenuModule,
+  ],
 })
 export class SharedModule {}
