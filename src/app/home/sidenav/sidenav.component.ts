39b679253a30fdf31edbdaae0d03220372c30928
Michael Cagampan <johnmichaelronn.cagampan@concentrix.com>
2021-06-10 16:19:29 +0800

    sidenav list of projects under my vault

--- src/app/home/sidenav/sidenav.component.ts
+++ src/app/home/sidenav/sidenav.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { ProjectService } from 'src/app/core/project.service';
+import { VaultService } from 'src/app/core/vault.service';
 
 declare var $;
 
@@ -8,12 +11,48 @@ declare var $;
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent implements OnInit, OnDestroy {
   selectedNav: string;
+  folders = [];
 
-  constructor(private renderer: Renderer2, private router: Router) {}
+  subscriptions: Subscription[] = [];
+  constructor(
+    private renderer: Renderer2,
+    private router: Router,
+    private ProjectService: ProjectService,
+    private VaultService: VaultService
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.subscriptions.push(
+      this.ProjectService.getProjects().subscribe((response: any) => {
+        const ids = [];
+        response.projects.forEach((p) => {
+          ids.push(p.project_id);
+        });
+
+        this.subscriptions.push(
+          this.VaultService.getFolders(ids).subscribe((response: any) => {
+            this.folders = [];
+            response.results.forEach((result) => {
+              result.forEach((res) => {
+                let folders = res
+                  .split('/')
+                  .filter((v) => v !== '' && v !== 'projects');
+
+                if (this.folders.indexOf(folders[0]) === -1)
+                  this.folders.push(folders[0]);
+              });
+            });
+          })
+        );
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((s) => s.unsubscribe());
+  }
 
   sidenavOverlayClicked() {
     // this.StateService.sidenavStateUpdated(false);
